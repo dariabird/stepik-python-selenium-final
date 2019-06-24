156b34efa9c04303f29f36a44983d9394d7151ac
Daria Lopteva <daria.lopteva@abbyy.com>
2019-06-24 16:36:00 +0300

    Added locators and checks for LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,8 @@ from selenium.webdriver.common.by import By
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
+class LoginPageLocators(object):
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
+
