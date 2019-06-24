919b6740be3e9859e16efacbbc8be6a2522606be
Daria Lopteva <daria.lopteva@abbyy.com>
2019-06-24 16:38:24 +0300

    Added messages for asserts in checks on the Login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,10 +8,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.url.contains("/login")
+        assert self.url.contains("/login"), "No 'login' in url of the Login page"
 
     def should_be_login_form(self):
-        assert self.is_element_present(self, *LoginPage.LOGIN_FORM)
+        assert self.is_element_present(self, *LoginPage.LOGIN_FORM), "No login form present on the Login page"
 
     def should_be_register_form(self):
-        assert self.is_element_present(self, *LoginPage.REGISTER_FORM)
+        assert self.is_element_present(self, *LoginPage.REGISTER_FORM), "No register form present on the Login page"
